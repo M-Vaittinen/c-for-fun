68953f906f3c5b6658690a4b457fe0e32b624dcf
Matti Vaittinen <matti.vaittinen@fi.rohmeurope.com>
2021-11-18 18:51:13 +0200

    add server thread creation

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ SRC = main.c points.c paikka.c alus.c powerup.c seina.c areena.c tekstit.c helpe
 HDR = alus.h areena.h common.h helpers.h hiscore.h media.h paikka.h piirrettavat_tekstit.h powerup.h seina.h server.h
 
 all: $(SRC) $(HDR)
-	$(CC) -Wall -ggdb `sdl2-config --cflags` $(SRC) -o ampu -lSDL2 -lm -lSDL2_ttf -lSDL2_mixer
+	$(CC) -Wall -ggdb `sdl2-config --cflags` $(SRC) -o ampu -lSDL2 -lm -lSDL2_ttf -lSDL2_mixer -lpthread
 	cp snd/* /home/mvaittin/.kolomiosnd/.
 install:
 	cp ampu /usr/bin/.
--- server.c
+++ server.c
@@ -1,7 +1,57 @@
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "server.h"
 
+struct server_thread_args {
+	struct server s;
+};
+
+
+static struct server_thread_args g_sa;
+
+void *server_thread(void *param)
+{
+	struct server_thread_args *a = (struct server_thread_args *)param;
+	int sock;
+
+	if (!a) {
+		/* Forcibly exit so the client won't stay up all alone */
+		printf("NULL server thread ard\n");
+		exit(-EINVAL);
+	}
+
+	sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (sock == -1) {
+		perror("socket\n");
+		exit(1);
+	} else {
+		printf("Socket %d created\n", sock);
+	}
+
+	for (;;) {
+		printf("thread running - ip %s\n", a->s.ip);
+		sleep(1);
+	}
+
+	return NULL;
+}
 
 int server_start(struct server *s)
 {
-	return 0;
+	int ret;
+	pthread_t tid;
+	pthread_attr_t attr;
+
+	g_sa.s = *s;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+
+	ret = pthread_create(&tid, &attr, server_thread, &g_sa);
+	if (!ret)
+		printf("Thread creation failed %d\n", ret);
+
+	return ret;
 }
