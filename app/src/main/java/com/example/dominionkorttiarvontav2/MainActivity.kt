3f462bc25a0ea2e68ecf1007fa93c17964968c6c
Matti Vaittinen <mazziesaccount@gmail.com>
2026-01-08 11:12:06 +0200

    Display custom error when network is down
    The HttpError won't be emitted if network is down, before the error occurs before HTTP has a chance to kick in.
    
    Add own onReceivedError() handler, which checks the network state and displays the custom error page if the network is down.
    
    Signed-off-by: Matti Vaittinen <mazziesaccount@gmail.com>

--- app/src/main/java/com/example/dominionkorttiarvontav2/MainActivity.kt
+++ app/src/main/java/com/example/dominionkorttiarvontav2/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.dominionkorttiarvontav2
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
 import androidx.activity.enableEdgeToEdge
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 
-//import androidx.core.view.ViewCompat
-//import androidx.core.view.WindowInsetsCompat
-
 class MainActivity : AppCompatActivity() {
+/*    private lateinit var myWebView: WebView */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,7 +27,8 @@ class MainActivity : AppCompatActivity() {
         webView.loadUrl("https://suffle.fi")
         webView.settings.javaScriptEnabled = true
 
-        onBackPressedDispatcher.addCallback(this,
+        onBackPressedDispatcher.addCallback(
+            this,
             object : OnBackPressedCallback(true) {
                 override fun handleOnBackPressed() {
                     val webView = findViewById<WebView>(R.id.web)
@@ -34,14 +41,36 @@ class MainActivity : AppCompatActivity() {
             }
         )
 
-        webView.webViewClient = WebViewClient()
+        webView.webViewClient = object : WebViewClient() {
+            override fun onReceivedHttpError(
+                view: WebView,
+                request: WebResourceRequest,
+                errorResponse: WebResourceResponse
+            ) {
+                if (request.url.equals("https://suffle.fi") ||
+                    request.url.equals("https://suffle.fi/index.php") ||
+                    request.url.equals("https://suffle.fi/aloittaja.php")
+                ) {
+                    view.loadUrl("file:///android_asset/error_page.html")
+                }
+            }
+            override fun onReceivedError(view: WebView, request: WebResourceRequest, error: WebResourceError) {
+                if (!isNetworkAvailable()) {
+                    view.loadUrl("file:///android_asset/error_page.html")
+                }
+            }
+            private fun isNetworkAvailable(): Boolean {
+                val connectivityManager =
+                    getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-        /*
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+                val networkCapabilities =
+                    connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+                return networkCapabilities != null && (
+                        networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+                        )
 
-        }*/
+            }
+        }
     }
 }
