8783ff2bd4a3355a406fe65d89b480a127ec7e06
Matti Vaittinen <mazziesaccount@gmail.com>
2026-01-21 13:13:04 +0200

    dominonsetit: Add a page displaying rated sets
    Signed-off-by: Matti Vaittinen <mazziesaccount@gmail.com>

---
+++ web/dominionsetit.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * Dominon card randomizer.
+ * Uses MySQL database.
+ *
+ * AUTHOR: Matti Vaittinen <mazziesaccount@gmail.com>
+ *
+ * Written just for fun. No Warranty. Use at your own risk!
+ * Copyright 2026, Matti Vaittinen mazziesaccount@gmail.com>
+ */
+
+define("MVAROOTPATH", "");
+
+require 'include/common.php';
+require 'include/db.php';
+require 'include/header.php';
+require 'include/nav_bar.php';
+require 'include/rating.php';
+
+do_head("Dominion - arvostellut korttisetit");
+$out = get_navlist(); 
+
+$out .= '<h1>Muiden pelaamia settej&auml</h1>'."\n";
+
+$out .= '<h3>Viimeisimm&auml;t arvostelut</h3>'."\n";
+$links = get_latest_rates($conn, 5);
+if (!$links) {
+	$out .= 'Hups... En saanut arvosteluja haettua.';
+} else {
+	$out .= '<ul>'."\n";
+	foreach($links AS $link) {
+		$out .= "<li>".$link."</li>\n";
+	}
+	$out .= "</ul>\n";
+}
+$out .= '<h3>Viimeksi lis&auml;tyt korttisetit</h3>'."\n";
+
+$links = get_latest_sets_and_rates($conn);
+if (!$links){
+	$out .= 'Hups... En saanut arvosteluja haettua.';
+} else{
+	$out .= '<ul>'."\n";
+	foreach($links AS $link) {
+		$out .= "<li>".$link."</li>\n";
+	}
+	$out .= '</ul>'."\n";
+}
+
+echo $out;
+include 'include/footer.php';
+generate_footer(false, true, true);
+
+?>
+
