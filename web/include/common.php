e967e0842bfcb9396ac378ebc468ae5a28bcce76
Matti Vaittinen <mazziesaccount@gmail.com>
2026-01-21 12:56:01 +0200

    Move commonly useful functions
    The dominion_common.php contains lots of randomizer specific common
    functionality. This is not needed elsewhere on the site, and including
    the unused functions make php handling heavier and collisions easier.
    Hence, it makes no sense to include dominion_common.php to all pages.
    
    However, there are some functions (like POST/GET data validation, debug
    printing etc), which may be relevant for other pages.
    
    Move commonly useful functions from dominion_common.php to common.php so
    that the pages can use them witout needing to include randomizer
    specific functions from the dominion_common.php
    
    Signed-off-by: Matti Vaittinen <mazziesaccount@gmail.com>

--- web/include/common.php
+++ web/include/common.php
@@ -5,7 +5,87 @@ define("OMENA_ID_OFFSET", 3000000);
 define("SET_RATE_MIN", -10);
 define("SET_RATE_MAX", 10);
 
-$DBG=true;
-//$DBG=false;
+//$DBG=true;
+$DBG=false;
+
+function return_post_numeric_array_or_ret($name, $ret)
+{
+	if (isset($_POST[$name]) && is_array($_POST[$name])) {
+		$arr = $_POST[$name];
+		foreach($arr AS $a)
+			if (!is_numeric($a))
+				return $ret;
+
+		return $arr;
+	}
+
+	return $ret;
+}
+
+function return_post_numeric_array_or_zero($name)
+{
+	return return_post_numeric_array_or_ret($name, 0);
+}
+
+function die_and_debug($msg)
+{
+	debug_print($msg);
+	die("Error in sent data");
+}
+
+function check_post_numeric_die($name, $min, $max)
+{
+	if (!isset($_POST[$name]))
+		die_and_debug("$name not set");
+
+	$val = $_POST[$name];
+
+	if (!is_numeric($val))
+		die_and_debug("Non numeric $name");
+
+	if ($min != null)
+		if ($val < $min)
+			die_and_debug("$name too small, $val. Limit $min");
+
+	if ($max != null)
+		if ($val > $max)
+			die_and_debug("$name too large, $val. Limit $max");
+
+	return $val;
+}
+
+function return_post_numeric_or_ret($name, $min, $max, $ret)
+{
+	if (!isset($_POST[$name]))
+		return $ret;
+
+	if (!is_numeric($_POST[$name]))
+		return $ret;
+
+	if ($min != null && $_POST[$name] < $min)
+		return $ret;
+
+	if ($max != null && $_POST[$name] > $max)
+		return $ret;
+
+	return $_POST[$name];
+}
+
+function return_post_numeric_or_zero($name, $min, $max)
+{
+	return return_post_numeric_or_ret($name, $min, $max, 0);
+}
+
+function debug_print($str)
+{
+	global $DBG;
+
+	if (!$DBG)
+		return;
+
+	echo "$str.<br />";
+}
+
+
 
 ?>
--- web/include/dominion_common.php
+++ web/include/dominion_common.php
@@ -12,73 +12,7 @@
 
 $CARDTYPE_MONEY = 1;
 
-function return_post_numeric_array_or_ret($name, $ret)
-{
-	if (isset($_POST[$name]) && is_array($_POST[$name])) {
-		$arr = $_POST[$name];
-		foreach($arr AS $a)
-			if (!is_numeric($a))
-				return $ret;
-
-		return $arr;
-	}
-
-	return $ret;
-}
-
-function return_post_numeric_array_or_zero($name)
-{
-	return return_post_numeric_array_or_ret($name, 0);
-}
-
-function die_and_debug($msg)
-{
-	debug_print($msg);
-	die("Error in sent data");
-}
-
-function check_post_numeric_die($name, $min, $max)
-{
-	if (!isset($_POST[$name]))
-		die_and_debug("$name not set");
-
-	$val = $_POST[$name];
-
-	if (!is_numeric($val))
-		die_and_debug("Non numeric $name");
-
-	if ($min != null)
-		if ($val < $min)
-			die_and_debug("$name too small, $val. Limit $min");
-
-	if ($max != null)
-		if ($val > $max)
-			die_and_debug("$name too large, $val. Limit $max");
-
-	return $val;
-}
-
-function return_post_numeric_or_ret($name, $min, $max, $ret)
-{
-	if (!isset($_POST[$name]))
-		return $ret;
-
-	if (!is_numeric($_POST[$name]))
-		return $ret;
-
-	if ($min != null && $_POST[$name] < $min)
-		return $ret;
-
-	if ($max != null && $_POST[$name] > $max)
-		return $ret;
-
-	return $_POST[$name];
-}
-
-function return_post_numeric_or_zero($name, $min, $max)
-{
-	return return_post_numeric_or_ret($name, $min, $max, 0);
-}
+require_once MVAROOTPATH.'include/common.php';
 
 function image_with_explanation($imgfile, $explanation, $imgalt = '', $css_class = '')
 {
@@ -93,16 +27,6 @@ function image_with_explanation($imgfile, $explanation, $imgalt = '', $css_class
 	$out .= '</div>'."\n";
 
 	return $out;
-}
-
-function debug_print($str)
-{
-	global $DBG;
-
-	if (!$DBG)
-		return;
-
-	echo "$str.<br />";
 }
 
 function get_expansions($conn, $include_nocards = false, $plain = false)
