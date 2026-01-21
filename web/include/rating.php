43071dcd3a0848a831bb50d31c60972947784513
Matti Vaittinen <mazziesaccount@gmail.com>
2026-01-21 13:06:05 +0200

    rating.php: Fix SQL generation and add functionality
    The store_rates() produces malformed SQL when a card set with
    events/landmarks/prophecies is being rated. The first 'AND' was missed
    for these WHERE constraints.
    
    Fix the SQL
    
    Also,
    
    Add new functions for fetching the information about latest sets. Add
    also a function which can be used to build custom links (for navigation)
    when last rates are displayed
    
    Signed-off-by: Matti Vaittinen <mazziesaccount@gmail.com>
    
    ---
    This should be split to a small fix commit (the store_rates() changes)
    and to larger patch adding new functionality.

--- web/include/rating.php
+++ web/include/rating.php
@@ -9,7 +9,255 @@
  * Copyright 2026, Matti Vaittinen mazziesaccount@gmail.com>
  */
 
-require 'include/ratebtn.php';
+require MVAROOTPATH.'include/ratebtn.php';
+require_once MVAROOTPATH.'include/common.php';
+
+function create_seturl_from_row($conn, $row)
+{
+	/*	$url = "https://suffle.fi/dominionarvonta.php?"; */
+	$url ="";
+	$kingdom_fields = array('card0', 'card1', 'card2', 'card3', 'card4', 'card5', 'card6', 'card7', 'card8', 'card9');
+	$land_fields = array('land0', 'land1');
+	$event_fields = array('event0', 'event1');
+	$propfield = 'prophecy';
+
+	$query = "SELECT prize, id FROM cards WHERE id = ".$row[$kingdom_fields[0]];
+
+	for ($i = 1; $i < 10; $i++)
+		$query .= " OR id = ".$row[$kingdom_fields[$i]];
+
+	$result = mysqli_query($conn, $query);
+	if (!$result)
+		die_and_debug("ratequery: $query".mysqli_error($conn));
+
+	if ($numres = mysqli_num_rows($result) != 10) {
+		debug_print("expected 10 results, got $numres");
+		return false;
+	}
+
+	while ($prizerow = mysqli_fetch_assoc($result)) {
+		if (!isset($prizerow['id']) || !isset($prizerow['prize'])) {
+			debug_print("ID / prize not found\n");
+			return false;
+		}
+
+		$id = $prizerow['id'];
+		$prize = $prizerow['prize'];
+		debug_print("ID: '".$id."'");
+		debug_print("prize: '".$prize."'");
+
+		if (!is_numeric($id) || $id == 0) {
+			debug_print("Malformed ID!\n");
+			return false;
+		}
+		if ($url == "")
+			$url = MVAROOTPATH.'/dominionarvonta.php?keepid[]='.$id.'&keepprize'.$id.'='.$prize;
+		else
+			$url .= '&keepid[]='.$id.'&keepprize'.$id.'='.$prize;
+
+		/*	debug_print("URL kierroksella $i: '".$url."'"); */
+	}
+	foreach ($land_fields AS $field) {
+		$id = $row[$field];
+		if ($id)
+			$url .= '&keepid[]='.$id + LAND_ID_OFFSET;
+	}
+	foreach ($event_fields AS $field) {
+		$id = $row[$field];
+		if ($id)
+			$url .= '&keepid[]='.$id + EVENT_ID_OFFSET;
+	}
+	if ($id = $row[$propfield])
+		$url .= '&keepid[]='.$id + OMENA_ID_OFFSET;
+
+	debug_print("returning URL $url\n");
+
+	return $url;
+}
+
+function get_rate_desc_avg($rate)
+{
+	if ($rate < -7.5)
+		return "1 t&auml;hti - Sysipaska";
+	if ($rate < -2.5)
+		return "2 t&auml;hte&auml - Huono";
+	if ($rate < 2.5)
+		return "3 t&auml;hte&auml - Ok";
+	if ($rate < 7.5)
+		return "4 t&auml;hte&auml - Hyv&auml;";
+
+	return "5 t&auml;hte&auml - Mahtava";
+}
+
+function get_rate_desc_from_row($row)
+{
+	switch ($row['rating']) {
+		case -10:
+			return "1 t&auml;hti - Sysipaska";
+		case -5:
+			return "2 t&auml;hte&auml - Huono";
+		case 0:
+			return "3 t&auml;hte&auml - Ok";
+		case 5:
+			return "4 t&auml;hte&auml - Hyv&auml;";
+		case 10:
+			return "5 t&auml;hte&auml - Mahtava";
+		default:
+			debug_print("Bad set rating ".$row['rating']);
+	}
+
+	return false;
+}
+/*
+define("LINK_ITEM_RATEDESC", "1");
+define("LINK_ITEM_RATE_ONLY", "2");
+define("LINK_ITEM_NUMVOTES", "4");
+ */
+/* TODO: Make it possible to select what info is added in link desc */
+function get_link_desc_from_row($row)
+{
+	$ratedesc = get_rate_desc_from_row($row);
+	if (!$ratedesc)
+		return false;
+
+	return $ratedesc." (".$row['numrates']." &auml;&auml;nest&auml;j&auml;&auml; t&auml;t&auml; mielt&auml;)";
+}
+
+function get_link_from_row($conn, $row)
+{
+	$url = create_seturl_from_row($conn, $row);
+	if (!$url)
+		return false;
+
+	$link_desc = get_link_desc_from_row($row);
+	if (!$link_desc)
+		return false;
+
+	$link = '<a href="'.$url.'">'.$link_desc."</a>\n";
+
+	return $link;
+}
+
+function rate2stars($rate)
+{
+	/* Rate from 0 => 20 */
+	$rate += 10;
+
+	/* Stars 0 to 4 */
+	$stars = $rate * 4 / 20;
+
+	/* Stars 1 to 5 */
+	$stars += 1;
+
+	/* Sanity in case input was insane ... */
+	if ($stars < 1)
+		$stars = 1;
+	if ($stars > 5)
+		$stars = 5;
+
+	return $stars;
+}
+
+function get_latest_sets_and_rates($conn, $numlatest = 5)
+{
+	$query = "SELECT * FROM cardsets ORDER BY added DESC LIMIT $numlatest";
+	$links = null;
+
+	$result = mysqli_query($conn, $query);
+	if (!$result) {
+		debug_print("setquery: $query".mysqli_error($conn));
+		return false;
+	}
+
+	while ($row = mysqli_fetch_assoc($result)) {
+		$query = "SELECT rating, numrates FROM setratings WHERE setid = ".$row['id'];
+
+		$result2 = mysqli_query($conn, $query);
+		if (!$result2) {
+			debug_print("setratingquery: $query".mysqli_error($conn));
+			continue;
+		}
+
+		$seturl = create_seturl_from_row($conn, $row);
+		if (!$seturl) {
+			debug_print("Failed to create URL for set ".$row['id']);
+			continue;
+		}
+		$accum_rate = 0;
+		$num_votes = 0;
+		while ($row2 = mysqli_fetch_assoc($result2)) {
+			$accum_rate += $row2['rating'] * $row2['numrates'];
+			$num_votes += $row2['numrates'];
+		}
+		if (!$num_votes)
+			continue;
+
+		$avg = $accum_rate / $num_votes;
+		$stars = rate2stars($avg);
+		$link_desc = number_format($stars, 2, ",")." (".get_rate_desc_avg($avg).") - $num_votes &auml;&auml;nt&auml;";
+		$links[] = '<a href="'.$seturl.'">'.$link_desc."</a>\n";
+	}
+
+	return $links;
+}
+
+function query_last_rates($conn, $numlatest)
+{
+	$query = "SELECT sr.rating, sr.numrates, cset.card0, cset.card1, cset.card2, cset.card3, cset.card4, cset.card5, cset.card6, cset.card7, cset.card8, cset.card9, cset.land0, cset.land1, cset.event0, cset.event1, cset.prophecy FROM setratings AS sr LEFT JOIN cardsets AS cset ON sr.setid = cset.id ORDER BY sr.time DESC LIMIT $numlatest";
+
+	$result = mysqli_query($conn, $query);
+	if (!$result)
+		die_and_debug("ratequery: $query".mysqli_error($conn));
+
+	return $result;
+}
+
+function get_last_rates_noformat($conn, $numlatest = 10, &$urls, &$rate, &$votes)
+{
+	$result = query_last_rates($conn, $numlatest);
+	$numfound = 0;
+
+	while ($row = mysqli_fetch_assoc($result)) {
+		$url = create_seturl_from_row($conn, $row);
+		if (!$url) {
+			debug_print("Failed to create URL\n");
+			continue;
+		}
+		if (!isset($row['rating']) || $row['rating'] == null) {
+			debug_print("Rating not set\n");
+			continue;
+		}
+		if (!isset($row['numrates']) || !$row['numrates']) {
+			debug_print("Num rating not set\n");
+			continue;
+		}
+
+		$urls[] = $url;
+		$rate[] = $row['rating'];
+		$votes[] = $row['numrates'];
+
+		$numfound ++;
+	}
+
+	return $numfound;
+}
+
+function get_latest_rates($conn, $numlatest = 10)
+{
+	$links = null;
+	$result = query_last_rates($conn, $numlatest);
+
+	while ($row = mysqli_fetch_assoc($result)) {
+		$link = get_link_from_row($conn, $row);
+		if (!$link)
+			return false;
+		$links[] = $link;
+	}
+	if (!$links)
+		return false;
+
+	return $links;
+}
 
 function is_bot_spam()
 {
@@ -221,15 +469,22 @@ function store_rates($conn, $rate, $preselected, $keep_land_ids, $keep_event_ids
 		$query = 'SELECT id FROM cardsets WHERE';
 		$query .= add_rate_where_clause($ratecards, 'card', 10);
 
-		if ($keep_land_ids)
+		if ($keep_land_ids) {
+			$query .= " AND";
 			$query .= add_rate_where_clause($keep_land_ids, 'land', 2);
+		}
 
-		if ($keep_event_ids)
+		if ($keep_event_ids) {
+			$query .= " AND";
 			$query .= add_rate_where_clause($keep_event_ids, 'event', 2);
+		}
 
-		if ($keep_omena_ids)
+		if ($keep_omena_ids) {
+			$query .= " AND";
 			$query .= add_rate_where_clause($keep_omena_ids, 'prophecy', 1);
+		}
 
+		echo $query;
 		$result = mysqli_query($conn, $query);
 		if (!$result) {
 			debug_print($query.' '.mysql_error($conn));
